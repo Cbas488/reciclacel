ab835cdd8bd5ee30a7e5f9ce0772e5be4babff4b
Daŕio Sebastián Zárate Ceballos <zdario488@gmail.com>
2023-08-29 21:59:08 -0600

    feat: terminando seccion de servicios en home

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -12,6 +12,15 @@ module.exports = {
 				reciclacel_green: '#0DB14B'
 			}
 		},
+		screens: {
+			'sm': '567.98px',
+			'md': '767.98px',
+			'lg': '1023.98px',
+			'xl': '1279.98px',
+			'2xl': '1536px',
+			'3xl': '1920px',
+			'4xl': '2560px'
+		  }
 	},
 	plugins: [],
 }
